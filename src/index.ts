45357beb78f4b4b71c0d55d1c08d14bc59689e74
Joao Araujo <jmmaraujo55@gmail.com>
2024-10-27 18:02:15 -0300

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-export { default as Client } from "./lib/client"
-export { default as Server } from "./lib/server"
-export {
-  Controller,
-  Delete,
-  Get,
-  Head,
-  Options,
-  Patch,
-  Post,
-  Put
-} from "./lib/http/decorators"
+export { default as Client } from "./lib/client";
+export { default as Server } from "./lib/server";
+export { Request, Response, NextFunction as Next } from "express";
+export { Controller, Delete, Get, Head, Options, Patch, Post, Put } from "./lib/http/decorators";
